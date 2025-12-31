f897fdb19a869b3aee0e18332f11e2af5f0ba2e4
Jason Grant <jason@flexewebs.com>
2025-12-31 00:53:24 +0000

    Neatened up navigation styling

--- com/navigation.php
+++ com/navigation.php
@@ -1,15 +1,13 @@
 <header class="universeBack">
     <h1 class="possibilityFront">Silicon Surrey</h1>
     <nav class="universeBack">
-        <div class="container">
-            
-                <ul>
-                    <li><a href="about">About</a></li>
-                    <li><a href="people">People</a></li>
-                    <li><a href="diary">Diary</a></li>
-                    <li><a href="jobs">Jobs</a></li>            
-                </ul>
-            
+        <div class="container">            
+            <ul>
+                <li><a href="about">About</a></li>
+                <li><a href="people">People</a></li>
+                <li><a href="diary">Diary</a></li>
+                <li><a href="jobs">Jobs</a></li>            
+            </ul>
         </div>
     </nav>
 </header>
