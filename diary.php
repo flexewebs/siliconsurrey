11bb6450ef3e79aa83718e1a9d383c804c835f42
Jason Grant <jason@flexewebs.com>
2026-01-01 23:57:21 +0000

    First diary entry
    Unstyled, but it will do for now.

--- diary.php
+++ diary.php
@@ -3,6 +3,11 @@
     <?php include("com/navigation.php"); ?>
 
     <h1>Diary</h1>
-    <p class="flow-text">We are going to tell our story here with regular updates.</p>
+    <p class="flow-text">We start 2026 as a true New Year for Silicon Surrey. Website development is in full swing at the moment. Website is serving as 
+        a guiding structure to set up the processes and workflows for onboarding various stakeholders into the project. We are slowly gearing up to 
+        lift off into a more publicly facing promotional phase. Lots to do to start getting the cogs of the system moving forward. Exciting times ahead!
+        1st January 2026
+        <time datetime="2026-01-01">1 January 2026</time>
+    </p>
     
 <?php include("com/footer.php"); ?>
