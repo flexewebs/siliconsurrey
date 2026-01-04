c26c1b02261433bbbb7ec22f266a9c7d99846a9d
Jason Grant <jason@flexewebs.com>
2026-01-04 01:30:10 +0000

    Updated about story

--- about.php
+++ about.php
@@ -1,16 +1,16 @@
-<?php $pageClass = "about"; ?>
+<?php $pageClass = "about purposeBack"; ?> 
 <?php include("com/header.php"); ?>
     <?php include("com/navigation.php"); ?>
 
     <main>
         <div class="container">
         <h1>Story of Silicon Surrey</h1>
-        <p class="flow-text">Jason came up with the idea in 2023. Started doing research in 
+        <p class="flow-text">Jason came up with the Silicon Surrey idea some time in early 2023. He started carrying out research in 
             Surrey, speaking to key stakeholders and community members. Most people were strong 
             supporters of the idea. The challenge was how to develop it from there. Jason was
-            told that Surrey needs a driven and energetic person who can drive such an idea forward.
+            told that Surrey needs a driven and energetic person such as him who can drive such an idea forward.
             Jason kept networking in Surrey and searching for key allies to help him navigate the complex 
-            journey of developing the idea.</p>
+            journey of developing a distributed startup incubator.</p>
         </div>
     </main>
     
