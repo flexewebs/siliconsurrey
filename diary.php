520a164fff13d65417ea2b1d9599d4f331d478aa
Jason Grant <jason@flexewebs.com>
2026-01-04 01:22:30 +0000

    Updated diary

--- diary.php
+++ diary.php
@@ -5,10 +5,17 @@
     <div class="container">
         <h1>Diary</h1>
         <section>
+            <p class="flow-text">We continue to build our web presence and work on formalising the Silicon Surrey presentation, to enable various people to easily 
+                understand what we are working on and how they can get involved. Post New Year spirit is still on, so many are away from work. We are thinking that 
+                next week will be a good time to start reaching out to potential allies and supporters to start building momentum for Silicon Surrey in 2026.                
+                <time datetime="2026-01-03">Sat, 3 January 2026</time>
+            </p>
+        </section>
+        <section>
             <p class="flow-text">We start 2026 as a true New Year for Silicon Surrey. Website development is in full swing at the moment. Website is serving as 
                 a guiding structure to set up the processes and workflows for onboarding various stakeholders into the project. We are slowly gearing up to 
                 lift off into a more publicly facing promotional phase. Lots to do to start getting the cogs of the system moving forward. Exciting times ahead!                
-                <time datetime="2026-01-01">1 January 2026</time>
+                <time datetime="2026-01-01">Thu, 1 January 2026</time>
             </p>
         </section>
     </div>
