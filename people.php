dade13a788241aee0c4c2485fa2bf83f440b214a
Jason Grant <jason@flexewebs.com>
2026-01-05 03:28:57 +0000

    Stared the writeup for people page

--- people.php
+++ people.php
@@ -1,8 +1,21 @@
-<?php $pageClass = "people"; ?>
+<?php $pageClass = "people peopleBack"; ?>
 <?php include("com/header.php"); ?>
     <?php include("com/navigation.php"); ?>
 
+    <main>
+
+    <div class="container">
     <h1>People</h1>
-    <p class="flow-text">We will outline people we are looking to engage here: founders, investors, mentors, non-executive directors, corporations, universities, researchers, SMEs.</p>
+    <p class="flow-text">We will outline people we are looking to engage here: founders, investors, 
+        mentors, non-executive directors, corporations, universities, researchers, SMEs.</p>
+
+    <h2>Jason Grant - Founder</h2>
+    <p class="flow-text">Jason is the founder of Silicon Surrey. He has over 20 years experience in the technology sector, 
+        working with startups, scaleups and large corporates. He is passionate about fostering innovation and supporting the growth of the tech ecosystem in Surrey.
+        He's previously worked as a startup mentor at Microsoft Accelerator, Mass Challenge, Tech Starts UK, Jaguar Land Rover Innovation Hub, Amex Innovation Labs and
+        has spoken at numberous startup and business conferences worldwide. Jason is also early stage seed investor into a number of prominent startups such as Nothing, Marloe Watch
+        Company and many others.</p>
+    </main>
+    </div>
     
 <?php include("com/footer.php"); ?>
