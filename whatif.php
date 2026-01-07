657d570a8d363368846d3d17422348adcd7a82fd
Jason Grant <jason@flexewebs.com>
2026-01-07 15:14:53 +0000

    Started the vision article

---
+++ whatif.php
@@ -0,0 +1,29 @@
+<?php $pageClass = "whatif innovationBack"; ?>
+<?php include("com/header.php"); ?>
+    <?php include("com/navigation.php"); ?>
+
+    <main>
+    <div class="container">
+        <h1>Vision for Surrey 2035 ...</h1>
+        
+        <h2>Importance of dreaming</h2>
+        <p class="flow-text">Dreaming is a power that drives human progress. It's through our dreams that we access our 
+            deepest creativity and vision for the future. We're reclaiming the art of dreaming as a 
+            vital tool for innovation and positive change. The dreams of yesterday are being reimagined 
+            today - evolving into new, bold visions that inspire action and create real impact. This is the 
+            nature of innovation: building the future through intentional thinking and collaborative progress. 
+            Let's embrace dreaming now and transform our shared vision into tangible reality.</p>
+
+        <h2>Future authoring Surrey</h2>
+        <p class="flow-text">One of the powerful Design Thinking methods is future authoring. The concept of writing the future 
+            which the designer envisons or dreams up, as though it is already happened. It's real. This article is a future authored 
+            vision for Surrey in 2035, outlining all the aspects of life in Surrey that have been transformed through innovation,
+            technology, and community collaboration. Read it as though it is already 2035.
+        </p>
+
+        
+        
+    </div>
+    </main>
+    
+<?php include("com/footer.php"); ?>
