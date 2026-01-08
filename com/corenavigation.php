80cccb7361072e748ebb1c6c7111858d6ed80916
Jason Grant <jason@flexewebs.com>
2026-01-08 11:21:52 +0000

    Added vision link

--- com/corenavigation.php
+++ com/corenavigation.php
@@ -3,6 +3,7 @@
         <li><a href="about">About</a></li>
         <li><a href="people">People</a></li>
         <li><a href="diary">Diary</a></li>
+        <li><a href="vision">Vision 2035</a></li>
         <li><a href="jobs">Jobs</a></li>            
     </ul>    
 </nav>
