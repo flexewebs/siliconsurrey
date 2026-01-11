5e997b781d8114a9b1249691b8863ab753836857
Jason Grant <jason@flexewebs.com>
2026-01-11 01:00:09 +0000

    Continuing the vision statement

--- whatif.php
+++ whatif.php
@@ -7,6 +7,7 @@
         <h1>Vision for Surrey 2035 ...</h1>
         
         <h2>Importance of dreaming</h2>
+        <p class="flow-text">TLDR: Dreams drive realisation of our intended future.</p>
         <p class="flow-text">Dreaming is a power that drives human progress. It's through our dreams that we access our 
             deepest creativity and vision for the future. We're reclaiming the art of dreaming as a 
             vital tool for innovation and positive change. The dreams of yesterday are being reimagined 
@@ -15,13 +16,20 @@
             Let's embrace dreaming now and transform our shared vision into tangible reality.</p>
 
         <h2>Future authoring Surrey</h2>
+        <p class="flow-text">TLDR: Future authoring is the first step to creating our intended future.</p>
         <p class="flow-text">One of the powerful Design Thinking methods is future authoring. The concept of writing the future 
             which the designer envisons or dreams up, as though it is already happened. It's real. This article is a future authored 
             vision for Surrey in 2035, outlining all the aspects of life in Surrey that have been transformed through innovation,
             technology, and community collaboration. Read it as though it is already 2035.
         </p>
 
-        
+        <h2>What we need to remember</h2>
+        <p class="flow-text">TLDR: There are many areas in which we can make a difference.</p>
+        <p class="flow-text">We want to innovate in area of life which are pertinent to Surrey and inspired by Surrey. Life in 
+            Surrey is clean, healthy, connected, friendly, natural and versatile. We want to preserve and enhance these qualities
+            through further innovation. True breakthroughs will come from areas which initially feel weird, highly unusual and 
+            even seemingly irrelevant. Yet those are the areas which we need to explore the most.
+        </p>
         
     </div>
     </main>
