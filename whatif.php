37eaf53a7b86643cc8b727193fb6e4acd67d930a
Jason Grant <jason@flexewebs.com>
2026-01-14 21:34:03 +0000

    Improved layout of vision page

--- whatif.php
+++ whatif.php
@@ -6,37 +6,45 @@
     <div class="container">
         <h1>Vision for Surrey 2035 ...</h1>
         
+        <section>
         <h2>Importance of dreaming</h2>
-        <p class="flow-text">TLDR: Dreams drive realisation of our intended future.</p>
+        <h3>TLDR: Dreams drive realisation of our intended future.</h3>
         <p class="flow-text">Dreaming is a power that drives human progress. It's through our dreams that we access our 
             deepest creativity and vision for the future. We're reclaiming the art of dreaming as a 
             vital tool for innovation and positive change. The dreams of yesterday are being reimagined 
             today - evolving into new, bold visions that inspire action and create real impact. This is the 
             nature of innovation: building the future through intentional thinking and collaborative progress. 
             Let's embrace dreaming now and transform our shared vision into tangible reality.</p>
+        </section>
 
+        <section>
         <h2>Future authoring Surrey</h2>
-        <p class="flow-text">TLDR: Future authoring is the first step to creating our intended future.</p>
+        <h3>TLDR: Future authoring is the first step to creating our intended future.</h3>
         <p class="flow-text">One of the powerful Design Thinking methods is future authoring. The concept of writing the future 
             which the designer envisons or dreams up, as though it is already happened. It's real. This article is a future authored 
             vision for Surrey in 2035, outlining all the aspects of life in Surrey that have been transformed through innovation,
             technology, and community collaboration. Read it as though it is already 2035.
         </p>
+        </section>
 
-        <h2>What we need to remember</h2>
-        <p class="flow-text">TLDR: There are many areas in which we can make a difference.</p>
+        <section>
+        <h2>Let's remember ...</h2>
+        <h3>TLDR: There are many areas in which we can make a difference.</h3>
         <p class="flow-text">We want to innovate in area of life which are pertinent to Surrey and inspired by Surrey. Life in 
             Surrey is clean, healthy, connected, friendly, natural and versatile. We want to preserve and enhance these qualities
             through further innovation. True breakthroughs will come from areas which initially feel weird, highly unusual and 
             even seemingly irrelevant. Yet those are the areas which we need to explore the most.
         </p>
+        </section>
 
-        <h2>Areas of interest</h2>
-        <p class="flow-text">TLDR: We can focus on farming, nature, wellness, transportation, energy and community.</p>
+        <section>
+        <h2>Areas of interest</>
+        <h3>TLDR: We can focus on farming, nature, wellness, transportation, energy and community.</h3>
         <p class="flow-text">Distributed nature of Silicon Surrey means that we can also distribute what we focus on both in terms of 
             themes as well as implementation. Take AI for example. What if AI capacity was distributed across Surrey? What if farming products 
         were fully distributed across Surrey? What if wellness provision and capabilities were fully distributed across Surrey? This kind of thinking
     potentially brings about new kind of ideas around what seem like typical domains.</p>
+        </section>
         
     </div>
     </main>
